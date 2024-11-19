55e850571e15151345ec13209f84779fb515a431
Iamthecoderofgods <22mjames@strichardgwyn.school>
2024-11-19 19:00:17 +0000

    Add files via upload

---
+++ class5.py
@@ -0,0 +1,13 @@
+matrix = [[1,2,3,4,],[5,"Orange",7,8]]
+print(matrix)
+
+#rows
+print("number of rows =",len(matrix))
+
+#colums
+print("number of colums =",len(matrix[0]))
+
+
+
+print(matrix[1][1])
+
